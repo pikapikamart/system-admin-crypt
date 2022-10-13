60497c66ead29f6b87ac3de391e019aae05d2a5f
pikapikamart <itsmelight1@gmail.com>
2022-10-13 10:02:39 +0800

    add page for single nft and add nft services

---
+++ src/pages/nfts/[nft].tsx
@@ -0,0 +1,81 @@
+import { connectDatabase } from "@/src/server/database";
+import { NFT } from "@/src/server/models/nft.model";
+import { findMultipleNftService, findMultipleNftServicePopulator, findNftService, findNftServicePopulator } from "@/src/server/services/nft.service";
+import { 
+  GetStaticPaths, 
+  GetStaticPropsContext, 
+  InferGetStaticPropsType, 
+  NextPage } from "next";
+import { ParsedUrlQuery } from "querystring";
+
+
+const NFTPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ( { nft } ) => {
+ 
+  return (
+    // <Coin coin={ coin } />
+    <></>
+  )
+}
+
+
+export default NFTPage
+
+type Params = {
+  params: {
+    nft: string
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async() =>{
+  await connectDatabase()
+  const nfts = await findMultipleNftService(
+    {},
+    "-_id nftId",
+  )
+  const nftsIds = nfts.reduce((accu, cur) =>{
+    accu.push({
+      params: {
+        nft: cur.id
+      }
+    })
+
+    return accu
+  }, [] as Params[])
+
+  return {
+    paths: nftsIds,
+    fallback: false
+  }
+}
+
+type NFTParam = ParsedUrlQuery & {
+  coin: string
+}
+
+export const getStaticProps = async( context: GetStaticPropsContext ) => {
+  await connectDatabase()
+  const { nft } = context.params as NFTParam
+  const singleNft = await findNftServicePopulator(
+    { nftId: nft },
+    "-_id ",
+    {
+      path: "owner",
+      select: "-_id username"
+    }
+  )
+
+  if ( !singleNft ) {
+    return {
+      redirect: {
+        destination: "/communities",
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {
+      nft: JSON.parse(JSON.stringify(singleNft)) as NFT
+    }
+  }
+}
--- src/server/services/nft.service.ts
+++ src/server/services/nft.service.ts
@@ -1,13 +1,37 @@
-import { DocumentDefinition, FilterQuery, ProjectionType } from "mongoose";
+import { DocumentDefinition, FilterQuery, PopulateOptions, ProjectionType } from "mongoose";
 import { NFT, nftModel } from "../models/nft.model";
 
 
 export const findNftService = async( 
   query: FilterQuery<NFT>,
-  projection: ProjectionType<NFT> = "" ) => (
+  projection: ProjectionType<NFT> = "" 
+) => (
   nftModel.findOne(query, projection)
+)
+
+export const findNftServicePopulator = async( 
+  query: FilterQuery<NFT>,
+  projection: ProjectionType<NFT> = "" ,
+  populate: PopulateOptions
+) => (
+  nftModel.findOne(query, projection).populate(populate)
 )
 
 export const createNftService = async( nft: DocumentDefinition<NFT> ) => (
   nftModel.create(nft)
+)
+
+export const findMultipleNftService = async(
+  query: FilterQuery<NFT>,
+  projection: ProjectionType<NFT>,
+) => (
+  nftModel.find(query, projection)
+)
+
+export const findMultipleNftServicePopulator = async(
+  query: FilterQuery<NFT>,
+  projection: ProjectionType<NFT>,
+  populate: PopulateOptions
+) => (
+  nftModel.find(query, projection).populate(populate)
 )
